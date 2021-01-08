51a086d3158da615d7f9bd1ae495a1b38e969604
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2021-01-08 16:20:55 -0500

    [boot] extract constants

--- boot/boot.s
+++ boot/boot.s
@@ -4,11 +4,17 @@
 .equ BOOT_SIGNATURE_SIZE, 0x2
 .equ BOOT_SIGNATURE,      0xAA55
 
+.equ INT_BIOS_VIDEO,      0x10
+.equ INT_BIOS_VIDEO_TTY,  0x0E
+
 .equ HEX_ASCII_OFFSET,    0x37
 .equ DEC_ASCII_OFFSET,    0x30
 .equ HEX_DIGIT_BIT_MASK,  0x000F
 .equ HEX_DIGIT_LENGTH,    0x4
+.equ HEX_DIGIT_SIZE,      0x4
 .equ HEX_SIGN_OFFSET,     0x2
+
+.equ NULL_SIGN,           0x0
 
 .text
 
@@ -27,13 +33,13 @@ wait:
   jmp .
 
 print_str:
-  mov $0x0E, %ah
+  mov $INT_BIOS_VIDEO_TTY, %ah
 
 print_str_loop:
   lodsb
-  cmp   $0x0, %al
+  cmp   $NULL_SIGN, %al
   je    print_str_exit
-  int   $0x10
+  int   $INT_BIOS_VIDEO
   jmp   print_str_loop
 
 print_str_exit:
@@ -63,7 +69,7 @@ print_hex_digit:
   ret
 
 shift_digit:
-  shr   $4, %ax
+  shr   $HEX_DIGIT_SIZE, %ax
   loop  shift_digit
   ret
 
