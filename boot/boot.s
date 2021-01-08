9eedf12644f69945a8818714d28691b2fa3e6955
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2021-01-08 11:42:21 -0500

    [boot] format code

--- boot/boot.s
+++ boot/boot.s
@@ -1,33 +1,33 @@
 .code16
 
-.equ BOOT_SECTOR_SIZE, 512
+.equ BOOT_SECTOR_SIZE,    512
 .equ BOOT_SIGNATURE_SIZE, 2
-.equ BOOT_SIGNATURE, 0xaa55
+.equ BOOT_SIGNATURE,      0xaa55
 
 .text
 
 .global boot
 
 boot:
-  mov  $boot_message, %si
-  call print
-  jmp .
+  mov   $boot_message, %si
+  call  print_str
+  jmp   .
 
-print:
+print_str:
   mov $0x0e, %ah
 
-print_loop:
+print_str_loop:
   lodsb
-  cmp $0x0, %al
-  je print_exit
-  int $0x10
-  jmp print_loop
+  cmp   $0x0, %al
+  je    print_str_exit
+  int   $0x10
+  jmp   print_str_loop
 
-print_exit:
+print_str_exit:
   ret
 
 boot_message:
-  .asciz "Booting dokkan...\r\n"
+  .asciz  "Booting Dokkan...\r\n"
 
-  .space BOOT_SECTOR_SIZE - BOOT_SIGNATURE_SIZE - (. - boot)
-  .word BOOT_SIGNATURE
+  .space  BOOT_SECTOR_SIZE - BOOT_SIGNATURE_SIZE - (. - boot)
+  .word   BOOT_SIGNATURE
