2bc0a2bf90c6af7b105d9e1079cad310d225a9b4
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2021-01-10 18:07:14 -0500

    [boot] init protected mode

--- boot/boot.s
+++ boot/boot.s
@@ -1,14 +1,15 @@
 boot_sector:
   .code16
 
-  .equ BOOT_SECTOR_SIZE,      0x0200
-  .equ BOOT_SIGNATURE,        0xAA55
-  .equ BOOT_SIGNATURE_SIZE,   0x0002
-  .equ BOOT_STACK_LOCATION,   0x8000
-  .equ BOOT_KERNEL_LOCATION,  0x9000
-  .equ BOOT_KERNEL_SECTORS,   0x0002
-  .equ BOOT_DRIVE_INIT_VALUE, 0x00
-  .equ BOOT_ENABLE_GDT_FLAG,  0x00000001
+  .equ BOOT_SECTOR_SIZE,                0x0200
+  .equ BOOT_SIGNATURE,                  0xAA55
+  .equ BOOT_SIGNATURE_SIZE,             0x0002
+  .equ BOOT_STACK_LOCATION,             0x8000
+  .equ BOOT_STACK_PROTECTED_LOCATION,   0x00090000
+  .equ BOOT_KERNEL_LOCATION,            0x9000
+  .equ BOOT_KERNEL_SECTORS,             0x0002
+  .equ BOOT_DRIVE_INIT_VALUE,           0x00
+  .equ BOOT_ENABLE_GDT_FLAG,            0x00000001
 
   .text
 
@@ -63,23 +64,36 @@ load_debug_message:
 
   .include "boot/gdt.s"
 
-boot_hang:
-  hlt
-  jmp boot_hang
-
 boot_enter_protected:
   cli
   lgdt (gdt_descriptor)
   mov %cr0, %eax
   or $BOOT_ENABLE_GDT_FLAG, %eax
   mov %eax, %cr0
-  jmp $CODE_SEGMENT, $boot_protected
+  jmp $CODE_SEGMENT, $boot_init_protected
+
+boot_init_protected:
+  .code32
+
+  mov $DATA_SEGMENT, %ax
+
+  mov %ax, %ds
+  mov %ax, %ss
+  mov %ax, %es
+  mov %ax, %fs
+  mov %ax, %gs
+
+  mov $BOOT_STACK_PROTECTED_LOCATION, %ebp;
+  mov %ebp, %esp
+
   call boot_protected
 
 boot_protected:
-  .code32
-
   call boot_hang
+
+boot_hang:
+  hlt
+  jmp boot_hang
 
   .space BOOT_SECTOR_SIZE - (. - boot_protected)
   .space BOOT_SECTOR_SIZE
